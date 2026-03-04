b0876ee2439cd11efb05ed3a1406b6e8bc1ef016
Kevin Harrington <mad.hephaestus@gmail.com>
2026-03-04 16:41:20 -0500

    update to java 21

--- scripts/makeJars.sh
+++ scripts/makeJars.sh
@@ -1,3 +1,5 @@
+export JAVA_HOME=$HOME/bin/java21/
+rm -rf build
 mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DASSIMP_ENABLE=ON -DBUILD_SHARED_LIBS=ON -DMANIFOLD_DEBUG=ON -DMANIFOLD_EXPORT=ON -DMANIFOLD_PAR=NONE -DCMAKE_POLICY_VERSION_MINIMUM=3.5 .. && make
@@ -5,4 +7,4 @@ cd ..
 cd bindings/java
 
 mvn versions:set -DnewVersion=$(cat version.txt) --file pom.xml
-mvn package -Dos.classifier=linux-x86_64
+mvn package -Dos.classifier=all
