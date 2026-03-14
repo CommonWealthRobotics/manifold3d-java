60453996aafd7a73c8bd423367b628552bbd835a
Kevin Harrington <mad.hephaestus@gmail.com>
2026-03-14 18:08:32 -0400

    update the build script

--- bindings/java/scripts/linux/buildC.sh
+++ bindings/java/scripts/linux/buildC.sh
@@ -8,6 +8,9 @@ set -e
 cmake -DMANIFOLD_CROSS_SECTION=ON -DMANIFOLD_USE_BUILTIN_CLIPPER2=ON -DCMAKE_BUILD_TYPE=Release -DASSIMP_ENABLE=ON -DBUILD_SHARED_LIBS=OFF -DMANIFOLD_DEBUG=ON -DMANIFOLD_EXPORT=ON -DMANIFOLD_PAR=ON -DMANIFOLD_USE_BUILTIN_TBB=ON -DCMAKE_POLICY_VERSION_MINIMUM=3.5 ..
 make
 cd ..
-
-cp build/src/libmanifold.so bindings/java/src/main/resources/manifold3d/natives/linux-x86_64/
-cp build/bindings/c/libmanifoldc.so bindings/java/src/main/resources/manifold3d/natives/linux-x86_64/
+echo "Build Done! Now copying the binaries into the Java Build..."
+echo "Current directory is $PWD"
+ls -al .
+ls -al ./build/src/*
+cp ./build/src/libmanifold.so ./bindings/java/src/main/resources/manifold3d/natives/linux-x86_64/
+cp ./build/bindings/c/libmanifoldc.so ./bindings/java/src/main/resources/manifold3d/natives/linux-x86_64/
