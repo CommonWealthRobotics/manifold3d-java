88d47ec554116f1fabef73c99bdb4653f6d43245
Kevin Harrington <mad.hephaestus@gmail.com>
2026-03-15 16:14:07 -0400

    revert anything that would interfere with publishing ffor now

--- bindings/java/src/main/java/com/cadoodlecad/manifold/ManifoldBindings.java
+++ bindings/java/src/main/java/com/cadoodlecad/manifold/ManifoldBindings.java
@@ -41,15 +41,12 @@ public class ManifoldBindings {
 			if (os.contains("win")) {
 				platform = "win-" + arch;
 				extension = ".dll";
-				libName="lib"+libName;
 			} else if (os.contains("mac")) {
 				platform = "mac-" + arch;
 				extension = ".dylib";
-				libName="lib"+libName;
 			} else if (os.contains("nix") || os.contains("nux") || os.contains("aix")) {
 				platform = "linux-" + arch;
 				extension = ".so";
-				libName="lib"+libName;
 			} else {
 				throw new RuntimeException("Unsupported OS: " + os);
 			}
@@ -77,6 +74,12 @@ public class ManifoldBindings {
 				needsCopy = devFile.lastModified() > libFile.lastModified();
 
 			if (needsCopy) {
+//				if (devFile.exists()) {
+//					java.nio.file.Files.copy(devFile.toPath(), libFile.toPath(), 
+//						java.nio.file.StandardCopyOption.REPLACE_EXISTING,
+//						java.nio.file.StandardCopyOption.COPY_ATTRIBUTES);
+//						libFile.setExecutable(true);
+//				} else {
 				try (java.io.InputStream in = ManifoldBindings.class
 						.getResourceAsStream("/manifold3d/natives/" + platform + "/" + fullName)) {
 					if (in == null)
@@ -86,10 +89,8 @@ public class ManifoldBindings {
 					System.out.println("Extracted to libs/: " + fullName);
 					if (cacheDirectory == null)
 						libFile.deleteOnExit();
-				}catch(Throwable t) {
-					throw new RuntimeException("Failed to load: " + libName, t);
-
 				}
+				// }
 			} else {
 				System.out.println("Copy not performed, already in cache");
 			}
@@ -102,8 +103,8 @@ public class ManifoldBindings {
 	}
 	
 	public static void loadNativeLibrarys(File cacheDirectory) throws Exception {
-		loadNativeLibrary("manifold", cacheDirectory);
-		loadNativeLibrary("manifoldc", cacheDirectory);
+		loadNativeLibrary("libmanifold", cacheDirectory);
+		loadNativeLibrary("libmanifoldc", cacheDirectory);
 	}
 
 	public ManifoldBindings() throws Exception {
@@ -112,8 +113,8 @@ public class ManifoldBindings {
 
 	public ManifoldBindings(File cacheDirectory) throws Exception {
 		if (!isNativeLibraryLoaded()) {
-			loadNativeLibrary("manifold", cacheDirectory);
-			loadNativeLibrary("manifoldc", cacheDirectory);
+			loadNativeLibrary("libmanifold", cacheDirectory);
+			loadNativeLibrary("libmanifoldc", cacheDirectory);
 		}
 
 		this.library = SymbolLookup.loaderLookup();
