9e227c8753c233a758d6540a73a757da9216e142
Kevin Harrington <mad.hephaestus@gmail.com>
2026-03-15 11:21:34 -0400

    Adding the arm support?

---
+++ bindings/java/scripts/linux/buildC-arm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#rm -rf build
+mkdir build
+cd build
+set -e
+cmake -DMANIFOLD_CROSS_SECTION=ON -DMANIFOLD_USE_BUILTIN_CLIPPER2=ON -DCMAKE_BUILD_TYPE=Release -DASSIMP_ENABLE=ON -DBUILD_SHARED_LIBS=ON -DMANIFOLD_DEBUG=ON -DMANIFOLD_EXPORT=ON -DMANIFOLD_PAR=ON -DMANIFOLD_USE_BUILTIN_TBB=ON -DCMAKE_POLICY_VERSION_MINIMUM=3.5 ..
+make
+cd ..
+echo "Build Done! Now copying the binaries into the Java Build..."
+echo "Current directory is $PWD"
+ls -al .
+ls -al ./build/src/*
+mkdir -p ./bindings/java/src/main/resources/manifold3d/natives/linux-arm64/
+cp ./build/src/libmanifold.so ./bindings/java/src/main/resources/manifold3d/natives/linux-arm64/
+cp ./build/bindings/c/libmanifoldc.so ./bindings/java/src/main/resources/manifold3d/natives/linux-arm64/
