dae5a1a073abfaf2677bd1d97e93e63fc17c0820
Kevin Harrington <mad.hephaestus@gmail.com>
2026-03-15 11:46:31 -0400

    adding a basic smoke test to the build.

---
+++ bindings/java/src/test/java/com/example/NativeLibLoading.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import com.cadoodlecad.manifold.ManifoldBindings;
+
+class NativeLibLoading {
+
+	@Test
+	void test() throws Exception {
+		ManifoldBindings manifold = new ManifoldBindings();
+	}
+
+}
