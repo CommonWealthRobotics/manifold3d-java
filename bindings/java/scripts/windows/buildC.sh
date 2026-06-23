5b613c1849e6e200fd851f34c6fac176e989ef00
Kevin Harrington <mad.hephaestus@gmail.com>
2026-06-23 14:44:30 -0400

    remove the cd .. which is flakey on windows

--- bindings/java/scripts/windows/buildC.sh
+++ bindings/java/scripts/windows/buildC.sh
@@ -1,47 +1,9 @@
 #!/bin/bash
 
-set -e
-
-if [ ! -d clipper2 ]; then
-  git clone https://github.com/AngusJohnson/Clipper2.git clipper2
-fi
-
-cd clipper2
-git fetch --all --tags
-git checkout 46f639177fe418f9689e8ddb74f08a870c71f5b4
-cd ..
-if [ ! -d nanobind ]; then
-  git clone https://github.com/wjakob/nanobind.git nanobind
-fi
-
-cd nanobind
-git fetch --all --tags
-git checkout v2.12.0
-cd ..
-if [ ! -d tbb ]; then
-  git clone https://github.com/oneapi-src/oneTBB.git tbb
-fi
-
-cd tbb
-git fetch --all --tags
-git checkout v2022.3.0
-cd ..
-if [ ! -d gtest ]; then
-  git clone https://github.com/google/googletest.git gtest
-fi
-
-cd gtest
-git fetch --all --tags
-git checkout v1.17.0
-mkdir build && cd build
-cmake -DCMAKE_BUILD_TYPE=Release ..
-make
-sudo make install
-cd ../..
 
 #rm -rf build
 mkdir build
-cd build
+
 cmake \
   -DCMAKE_BUILD_TYPE=Release \
   -DBUILD_SHARED_LIBS=ON \
@@ -57,8 +19,8 @@ cmake \
   -DFETCHCONTENT_SOURCE_DIR_NANOBIND=../nanobind \
   -DFETCHCONTENT_SOURCE_DIR_GOOGLETEST=../gtest \
   -A x64 -B .
-cmake --build . --target ALL_BUILD --config Release
-cd ..
+cmake --build build --target ALL_BUILD --config Release
+
 mkdir -p ./bindings/java/src/main/resources/manifold3d/natives/win-x86_64/
 rm -rf ./bindings/java/src/main/resources/manifold3d/natives/win-x86_64/*
 cp ./build/lib/Release/manifold.dll ./bindings/java/src/main/resources/manifold3d/natives/win-x86_64/
